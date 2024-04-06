ff3dd61101dadbb6b7d9aab5649fbf9db32d8c8e
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2024-04-06 11:49:03 -0700

    Properly depend on internal transport-raknet
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- bedrock-connection/build.gradle.kts
+++ bedrock-connection/build.gradle.kts
@@ -1,3 +1,10 @@
+import org.gradle.plugins.ide.eclipse.model.AccessRule
+import org.gradle.plugins.ide.eclipse.model.AbstractClasspathEntry
+
+plugins {
+    id("eclipse")
+}
+
 dependencies {
     api(projects.bedrockCodec)
     api(libs.netty.transport.raknet)
@@ -8,4 +15,26 @@ tasks.jar {
     manifest {
         attributes("Automatic-Module-Name" to "org.cloudburstmc.protocol.bedrock.connection")
     }
+}
+
+eclipse {
+	classpath {
+		file {
+			whenMerged.add( object: Action<org.gradle.plugins.ide.eclipse.model.Classpath> {
+				override fun execute( classpath: org.gradle.plugins.ide.eclipse.model.Classpath ) {
+					val jre = classpath.entries.find {
+						it is AbstractClasspathEntry &&
+							it.path.contains("org.eclipse.jdt.launching.JRE_CONTAINER")
+					} as AbstractClasspathEntry
+
+					// make sun.misc accessible in Eclipse project
+					// (when refreshing Gradle project in buildship)
+					jre.accessRules.add(AccessRule("accessible", "sun/misc/**"))
+
+					// remove trailing slash from jre path
+					if (jre.path.endsWith("/")) jre.path = jre.path.substring(0, jre.path.length - 1)
+				}
+			} )
+		}
+	}
 }
